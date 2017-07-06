f1bde1c20d03bdd6f0c1ddfd9fd293d9557d82d1
Your Name <you@example.com>
2017-07-06 10:11:45 -0400

    initial commit

---
+++ copyArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copyArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loops
+            //For loops iterate through conditions and runs the loop until the condition is not valid
+            //Examples:
+            //Initialize arrays
+            int[] original = new int [10];
+            for (int i = 0; i < 10; i++)
+                original[i] = i * 3;
+            int[] copy = new int[original.Length];
+            //As long as variable i is less than the length of array original
+            //increment by 1
+            for (int i = 0; i < original.Length; i++)
+            //as each loop progresses make copy element i equals original element i
+                copy[i] = original[i];
+            //Print each element for each array
+            for (int i = 0; i < original.Length; i++)
+                Console.WriteLine(original[i] + "\t" + copy[i]);
+            for (int i = 50; i <= 100; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
